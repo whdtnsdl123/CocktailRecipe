4501058b846fc429154ce137dbaa95032425bf79
c282804 <c282804@gmail.com>
2020-07-15 14:53:44 +0900

    commit

--- app/src/main/java/com/c282804/cocktailrecipe/NormalFragment.java
+++ app/src/main/java/com/c282804/cocktailrecipe/NormalFragment.java
@@ -29,8 +29,8 @@ public class NormalFragment extends Fragment {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
 
-        items.add(new Item("롱아일랜드아이스티",R.drawable.milk,"http://blogfiles.naver.net/MjAxOTAxMjBfNDYg/MDAxNTQ3OTU5NTUzNjgx.3zeAVfnqLARnajZpTc3SyOyUb2eeDG3yyXf_v9X5RlMg.kyZxOpyxYsKLZaJB85m8cWy7f5HgccLXTml4Q5HtBYsg.JPEG.tjsxorud/%B4%D9%BF%EE%B7%CE%B5%E5.jpg"));
-        items.add(new Item("버번콕",R.drawable.rise,"https://lh3.googleusercontent.com/proxy/vLWR8YZC83SvtddTuHKWXZfYbXewvJ0Q-VE1mHyYrkwRAed9ezVVQl4-eB3KkTTS9slJZRF93XsRu6savlh_spBMdniLR5TLONTE_9qCZXrIwGeovH0ynjIqrlkonR-io9Q3_v5OpeVDayIF_FR1qAPc5DfTrG4cgoho9CypyEPiIlnSUycI2R0X5Odz1NCOGsOoLiKrWdAPvjOnohj9zCVNwogdj0FTjjOFJmBLAW6jiamcbCosJBnUur-e8KagwohoLFnAAFD9Ozmv9mIZRzqn4sGL7Rdv5ztljpzJfYeec92glaKDBJMgb5K8NfVwVHwcluj_f21Vw3TZinCPiHl9wH4CWR6XTxAK6bh3KmYQFAYOSNMvwN6pPUw1"));
+        items.add(new Item("롱아일랜드아이스티",R.drawable.longtee,"https://media.istockphoto.com/photos/long-island-iced-tea-picture-id180888927?k=6&m=180888927&s=170667a&w=0&h=EWPsZHys_ISiIJcSDWMBjRyT18R7M2u0SYwTHcNRwnU="));
+        items.add(new Item("레몬 드랍",R.drawable.rise,"https://lh3.googleusercontent.com/proxy/vLWR8YZC83SvtddTuHKWXZfYbXewvJ0Q-VE1mHyYrkwRAed9ezVVQl4-eB3KkTTS9slJZRF93XsRu6savlh_spBMdniLR5TLONTE_9qCZXrIwGeovH0ynjIqrlkonR-io9Q3_v5OpeVDayIF_FR1qAPc5DfTrG4cgoho9CypyEPiIlnSUycI2R0X5Odz1NCOGsOoLiKrWdAPvjOnohj9zCVNwogdj0FTjjOFJmBLAW6jiamcbCosJBnUur-e8KagwohoLFnAAFD9Ozmv9mIZRzqn4sGL7Rdv5ztljpzJfYeec92glaKDBJMgb5K8NfVwVHwcluj_f21Vw3TZinCPiHl9wH4CWR6XTxAK6bh3KmYQFAYOSNMvwN6pPUw1"));
         items.add(new Item("블루사파이어",R.drawable.rum,"https://img1.daumcdn.net/thumb/R720x0.q80/?scode=mtistory2&fname=http%3A%2F%2Fcfile3.uf.tistory.com%2Fimage%2F212C3342526088E908703E"));
         items.add(new Item("섹스온더비치",R.drawable.maribu,"https://dailyshotassets.s3-accelerate.amazonaws.com/media/cocktails/%25Y/%25m/%25d/9162c6d4-4df8-417c-a108-290dd9ccdb69.jpg"));
         items.add(new Item("잭콕",R.drawable.shower,"https://dailyshotassets.s3-accelerate.amazonaws.com/media/cocktails/%25Y/%25m/%25d/de34bbe5-8bdd-4149-960b-edae43cb1943.jpg"));
